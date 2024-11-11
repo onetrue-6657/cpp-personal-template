148a14aa609e7f13f57640ea8ecd4465cfae6cdb
One_True <60509431+onetrue-6657@users.noreply.github.com>
2024-11-10 22:25:20 -0500

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,16 @@
+/* Template */
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+
+int main()
+{
+    ios::sync_with_stdio(0); // No more scanf and printf
+    cin.tie(0);              // No more binding with cout
+    cout.tie(0);             // No more binding with cin
+
+    return 0;
+}
